e01d78a15296f384d7a331a0ad92574a3bdac913
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-07 15:11:17 +0000

    Update Review 4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,16 +13,31 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String num1;
+		num1 = in.nextLine();
+		num1 = (5 + num1);
+		System.out.print(Integer.parseInt(num1) + 5);
+		System.out.println();
+
 		
 	}
 
 	public static void q2() {
 		//Write question 2 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String num2;
+		num2 = in.nextLine();
+		num2 = (4 + num2 +3);
+		System.out.print(Double.parseDouble(num2) + 3.4);
+		System.out.println();
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+
 		
 	}
 
